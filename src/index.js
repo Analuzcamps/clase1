5b8ddc34eede5a2544cbb76ca46c516e219e3f4a
analuzcamps <anacoderhouse@gmail.com>
2022-06-01 18:02:34 -0200

    separamos el primer componente App

--- src/index.js
+++ src/index.js
@@ -1,4 +1,50 @@
+//Traigo la libreria de Ract
+
 import React from 'react';
+
+//Traigo la libreria de ReactDOM
+
+import ReactDOM from 'react-dom/client';
+
+//Agarro una librería que ya exista en React
+
+/*function App(){
+  return "Hola Mundo"
+}*/
+
+/*const App = () => "Hola Mundo usando JSX y Arrow Function"*/
+
+import App from "./App"
+
+//Mostrar al app en el DOM (crea root donde la app se va a mostrar)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -15,3 +61,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
